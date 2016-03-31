619734ea3ab47c4f83d89c6aff492c12cfa2b40f
Abdur Rehman <abdurrehmanamjad@gmail.com>
2016-04-01 03:43:32 +0500

    migration column text size limit changed for PG on heroku

--- db/migrate/20160331080415_create_user_contents.rb
+++ db/migrate/20160331080415_create_user_contents.rb
@@ -1,7 +1,7 @@
 class CreateUserContents < ActiveRecord::Migration
   def change
     create_table :user_contents do |t|
-      t.text :json_body,    limit: 4294967296
+      t.text :json_body,    limit: 1073741824
 
       t.timestamps null: false
     end
--- db/migrate/20160331204943_create_past_exports.rb
+++ db/migrate/20160331204943_create_past_exports.rb
@@ -2,7 +2,7 @@ class CreatePastExports < ActiveRecord::Migration
   def change
     create_table :past_exports do |t|
       t.integer :user_id
-      t.text :content,    limit: 4294967296
+      t.text :content,    limit: 1073741824
 
       t.timestamps null: false
     end
